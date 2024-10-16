b9dfa7822d3ee2beb7543eed3d832e2b599f5bd5
gen.java <132185718+GrizonRussell@users.noreply.github.com>
2024-10-16 15:24:44 +0800

    Update Step2_education_form.dart

--- lib/steps/Step2_education_form.dart
+++ lib/steps/Step2_education_form.dart
@@ -1,73 +1,91 @@
 // lib/steps/education_form.dart
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-class EducationForm extends StatelessWidget {
+class EducationForm extends StatefulWidget {
   final GlobalKey<FormState> formKey;
   final Map<String, String> formData;
+  final List<Map<String, String>> institutionList;
+  final List<Map<String, String>> courseList;
 
-  EducationForm({required this.formKey, required this.formData});
+  EducationForm({
+    required this.formKey,
+    required this.formData,
+    required this.institutionList,
+    required this.courseList,
+  });
+
+  @override
+  _EducationFormState createState() => _EducationFormState();
+}
+
+class _EducationFormState extends State<EducationForm> {
+  String? selectedInstitution;
+  String? selectedCourse;
+  DateTime? selectedGraduationDate;
 
   @override
   Widget build(BuildContext context) {
     return Form(
-      key: formKey,
+      key: widget.formKey,
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          _buildTextField(
-            'College Course',
-            'collegeCourse',
-            formData,
-            validator: (value) {
-              return value == null || value.isEmpty
-                  ? 'This field is required'
-                  : null;
+          // Institution Dropdown
+          _buildDropdownField(
+            label: 'Institution',
+            items: widget.institutionList,
+            value: selectedInstitution,
+            onChanged: (value) {
+              setState(() {
+                selectedInstitution = value;
+                widget.formData['institution'] = value!;
+              });
             },
           ),
-          _buildTextField(
-            'Graduate Course',
-            'graduateCourse',
-            formData,
-            validator: (value) {
-              return value == null || value.isEmpty
-                  ? 'This field is required'
-                  : null;
+
+          // Course Dropdown (dependent on Institution)
+          _buildDropdownField(
+            label: 'Course',
+            items: widget.courseList
+                .where((item) => item['institution'] == selectedInstitution)
+                .toList(),
+            value: selectedCourse,
+            onChanged: (value) {
+              setState(() {
+                selectedCourse = value;
+                widget.formData['course'] = value!;
+              });
             },
           ),
-          _buildTextField(
-            'Date of Graduation',
-            'graduationDate',
-            formData,
-            validator: (value) {
-              return value == null || value.isEmpty
-                  ? 'This field is required'
-                  : null;
+
+          // Date Graduated (with Calendar Picker)
+          _buildDatePickerField(
+            label: 'Date Graduated',
+            selectedDate: selectedGraduationDate,
+            onDateSelected: (date) {
+              setState(() {
+                selectedGraduationDate = date;
+                widget.formData['graduationDate'] =
+                    DateFormat('yyyy-MM-dd').format(date);
+              });
             },
           ),
-          _buildTextField(
-            'PRC License No.',
-            'prcLicenseNo',
-            formData,
-            validator: (value) {
-              return value == null || value.isEmpty
-                  ? 'This field is required'
-                  : null;
-            },
-          ),
-          SizedBox(height: 20), // Add some spacing
         ],
       ),
     );
   }
 
-  Widget _buildTextField(
-    String label,
-    String key,
-    Map<String, String> formData, {
-    FormFieldValidator<String>? validator,
+  // Helper method for Dropdown Field
+  Widget _buildDropdownField({
+    required String label,
+    required List<Map<String, String>> items,
+    String? value,
+    required ValueChanged<String?> onChanged,
   }) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: TextFormField(
+      child: DropdownButtonFormField<String>(
         decoration: InputDecoration(
           labelText: label,
           labelStyle: const TextStyle(color: Color.fromRGBO(255, 255, 255, 1)),
@@ -75,18 +93,85 @@ class EducationForm extends StatelessWidget {
           fillColor: const Color.fromARGB(255, 25, 189, 109).withOpacity(0.2),
           enabledBorder: OutlineInputBorder(
             borderSide: BorderSide(
-                color: const Color.fromRGBO(11, 134, 74, 1).withOpacity(0.5)),
+              color: const Color.fromRGBO(11, 134, 74, 1).withOpacity(0.5),
+            ),
           ),
           focusedBorder: const OutlineInputBorder(
             borderSide: BorderSide(color: Color.fromRGBO(11, 134, 74, 1)),
           ),
         ),
         style: const TextStyle(color: Colors.white),
-        validator: validator,
-        onSaved: (value) {
-          formData[key] = value ?? '';
+        value: value,
+        items: items.map((item) {
+          return DropdownMenuItem<String>(
+            value: item['value'],
+            child: Text(item['label']!),
+          );
+        }).toList(),
+        onChanged: onChanged,
+        validator: (value) {
+          return value == null || value.isEmpty
+              ? 'This field is required'
+              : null;
         },
       ),
     );
+  }
+
+  // Helper method for Date Picker Field
+  Widget _buildDatePickerField({
+    required String label,
+    DateTime? selectedDate,
+    required ValueChanged<DateTime> onDateSelected,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: TextFormField(
+        readOnly: true,
+        decoration: InputDecoration(
+          labelText: label,
+          labelStyle: const TextStyle(color: Color.fromRGBO(255, 255, 255, 1)),
+          filled: true,
+          fillColor: const Color.fromARGB(255, 25, 189, 109).withOpacity(0.2),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: const Color.fromRGBO(11, 134, 74, 1).withOpacity(0.5),
+            ),
+          ),
+          focusedBorder: const OutlineInputBorder(
+            borderSide: BorderSide(color: Color.fromRGBO(11, 134, 74, 1)),
+          ),
+          suffixIcon: IconButton(
+            icon: const Icon(Icons.calendar_today, color: Colors.white),
+            onPressed: () => _selectDate(context, onDateSelected),
+          ),
+        ),
+        style: const TextStyle(color: Colors.white),
+        controller: TextEditingController(
+          text: selectedDate == null
+              ? 'Select a date'
+              : DateFormat('MMMM dd, yyyy').format(selectedDate),
+        ),
+        validator: (value) {
+          return value == null || value.isEmpty
+              ? 'This field is required'
+              : null;
+        },
+      ),
+    );
+  }
+
+  // Method to show Date Picker
+  Future<void> _selectDate(
+      BuildContext context, ValueChanged<DateTime> onDateSelected) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: selectedGraduationDate ?? DateTime.now(),
+      firstDate: DateTime(1900),
+      lastDate: DateTime.now(),
+    );
+    if (picked != null && picked != selectedGraduationDate) {
+      onDateSelected(picked);
+    }
   }
 }
